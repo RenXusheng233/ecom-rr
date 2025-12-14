13cd66f16eafb616900585bc5bc18e2e2ea3b47a
RenXusheng233 <782449909@qq.com>
2025-12-14 23:04:51 +0800

    chore(service): product router

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,12 +1,15 @@
-import express, { Request, Response } from 'express'
-import { clerkClient, clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+import { clerkClient, clerkMiddleware } from '@clerk/express'
 import { ShouldBeUser } from './middleware/authMiddleware.js'
+import productRouter from './routes/product.route.js'
+import categoryRouter from './routes/category.route.js'
 
 const app = express()
 const port = 8000
 
 app.use(clerkMiddleware())
+app.use(express.json())
 
 app.use(
   cors({
@@ -27,6 +30,24 @@ app.get('/test', ShouldBeUser, async (req: Request, res: Response) => {
   const user = await clerkClient.users.getUser(req.userId)
   res.json({ message: 'User retrieved from product service', user })
 })
+
+app.use('/products', productRouter)
+app.use('/categories', categoryRouter)
+
+// Error Handler
+app.use(
+  (
+    err: { status: number; message: string },
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    console.log(err)
+    return res
+      .status(err.status || 500)
+      .json({ message: err.message || 'Internal Server Error!' })
+  },
+)
 
 app.listen(port, () => {
   console.log(`Product service is running on port ${port}`)
