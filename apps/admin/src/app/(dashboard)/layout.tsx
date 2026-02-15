0978b1f27361d3ec4eb86b16cf292a6572553a9e
RenXusheng233 <782449909@qq.com>
2026-02-15 19:55:13 +0800

    feat(admin): add clerk

---
+++ apps/admin/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import { cookies } from 'next/headers'
+import AppSideBar from '@/components/AppSideBar'
+import NavBar from '@/components/NavBar'
+import { ThemeProvider } from '@/components/providers/ThemeProvider'
+import { SidebarProvider } from '@/components/ui/sidebar'
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true'
+
+  return (
+    <div className="flex">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <AppSideBar />
+          <main className="w-full">
+            <NavBar />
+            <div className="px-4">{children}</div>
+          </main>
+        </SidebarProvider>
+      </ThemeProvider>
+    </div>
+  )
+}
--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,10 +1,10 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { ToastContainer } from 'react-toastify'
-import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
